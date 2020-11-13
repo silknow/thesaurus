1d0f0eb3a9eae934408ecd33ebc095529c80ac4e
tschleider <tommy1122000@gmail.com>
2020-11-13 15:05:29 +0100

    Added support for controlled vocabulary of P2_has_type

---
+++ converter_P2/prefixes.js
@@ -0,0 +1,24 @@
+import $rdf from 'rdflib';
+
+export const SILKNOW_P2 = $rdf.Namespace('http://data.silknow.org/has_type_P2/');
+export const FACET = $rdf.Namespace('http://data.silknow.org/vocabulary/facet/');
+export const RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+export const RDFS = $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#');
+export const SKOS = $rdf.Namespace('http://www.w3.org/2004/02/skos/core#');
+export const DC = $rdf.Namespace('http://purl.org/dc/terms/');
+export const XSD = $rdf.Namespace('http://www.w3.org/2001/XMLSchema#');
+export const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+export const PAV = $rdf.Namespace('http://purl.org/pav/');
+
+
+export const nsValues = {
+  silknow_p2: SILKNOW_P2().value,
+  'silknow-fct': FACET().value,
+  skos: SKOS().value,
+  dct: DC().value,
+  xsd: XSD().value,
+  getty: 'http://vocab.getty.edu/aat/',
+  rdfs: RDFS().value,
+  foaf: FOAF().value,
+  pav: PAV().value,
+};
---
+++ converter_P2/utils.js
@@ -0,0 +1,14 @@
+import $rdf from 'rdflib';
+import validUrl from 'valid-url';
+
+export const store = $rdf.graph();
+
+export function add(s, p, o, lang) {
+  if (!s || !p || !o) return;
+  /* eslint-disable no-param-reassign  */
+  if (typeof o === 'string') o = o.trim();
+  if (!o) return;
+  if (lang) store.add(s, p, $rdf.literal(o, lang));
+  else if (typeof o === 'string' && validUrl.isUri(o)) store.add(s, p, $rdf.sym(o));
+  else store.add(s, p, o);
+}
